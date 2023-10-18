8b11ce3745b4219760e5d6c1e2b869b5cbca91ee
Zheng Xiang <wong.zhengxiang@gmail.com>
2023-10-18 14:57:01 +0800

    ADDED: Comments

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,19 +6,19 @@
 /*   By: zwong <zwong@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 18:35:20 by zwong             #+#    #+#             */
-/*   Updated: 2023/07/28 15:43:52 by zwong            ###   ########.fr       */
+/*   Updated: 2023/10/18 14:56:23 by zwong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
-// Learning how to use iterator and algorithm logic
-// Span is basically just the range between a set of numbers
-// Hence, shortestSpan() will compare all the numbers and find the smallest difference in value
+// Span is basically just a range of numbers
+// shortestSpan() will compare all the numbers and find the smallest difference in value
+// shortestSpan() will compare all the numbers and find the largest difference in value
 int main() {
     std::cout << MAGENTA << "----- TESTING BASICS -----" << std::endl << RESET;
     std::cout << YELLOW << "Adding number into span..." << std::endl << RESET;
-    Span span = Span(10);
+    Span span = Span(10); // changes span size to test range limit N
     std::cout << GREEN << "Span created with size: " << span.size() << std::endl << RESET;
     try {
         span.addNumber(15);
@@ -49,6 +49,8 @@ int main() {
     std::cout << YELLOW << "Original span contents..." << std::endl << RESET;
     span.printContents();
     std::cout << YELLOW << "Copy span contents..." << std::endl << RESET;
+
+    // Compares copy span vs original span (copy has added a 42)
     copy.printContents();
     std::cout << GREEN << "YES, it is deep copy!" << std::endl << RESET;
     std::cout << std::endl;
@@ -62,7 +64,7 @@ int main() {
     }
     std::cout << YELLOW << "Filling with numbers from 0 to 15000..." << std::endl << RESET;
     span.fillNumbers(numbers.begin(), numbers.end()); // fill numbers from given vector range
-    span.printContents();
+    // span.printContents();
     std::cout << YELLOW << "Calculating shortest and longest span..." << std::endl << RESET;
     std::cout << GREEN << "Shortest span is: " << span.shortestSpan() << std::endl << RESET;
     std::cout << GREEN << "Longest span is: " << span.longestSpan() << std::endl << RESET;
